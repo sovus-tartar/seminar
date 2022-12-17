cf086590ad9cedc7664f80b22da38eaf960f3925
sovus-tartar <vad-nesmejanov@yandex.ru>
2022-12-17 15:22:19 +0300

    pastux final with daemons

--- pastux_exam_task/main.c
+++ pastux_exam_task/main.c
@@ -11,13 +11,13 @@
 #include <sys/shm.h>
 #include <sys/wait.h>
 
-#define TO_QUEUE 100000000
-#define LEAVE_QUEUE 100000001
+#define TO_QUEUE 1000000
+#define LEAVE_QUEUE 1000001
 
 int bridge_sem;
 
 int entry_sem;
-
+int end_sem;
 int *line_num_arr;
 int line_num_sem;
 
@@ -70,6 +70,12 @@ int queue_daemon(int side)
         struct msg_t rcv1;
         msgrcv(queue_id[side], &rcv1, sizeof(int), TO_QUEUE, 0);
 
+        if(rcv1.msg_data == LEAVE_QUEUE)
+        {
+            printf("queue_daemon %d died\n", side);
+            exit(0);
+        }
+
         struct msg_t snd = {rcv1.msg_data, 0};
 
         msgsnd(queue_id[side], &snd, sizeof(int), 0);
@@ -78,6 +84,14 @@ int queue_daemon(int side)
 
         msgrcv(queue_id[side], &rcv2, sizeof(int), LEAVE_QUEUE, 0);
     }
+}
+
+void kill_queue(int num)
+{
+    struct msg_t snd = {TO_QUEUE, LEAVE_QUEUE};
+    
+    msgsnd(queue_id[num], &snd, sizeof(int), 0);
+    printf("Msg send to %d\n", num);
 }
 
 int stand_to_queue(int num, int side)
@@ -101,6 +115,13 @@ void pastux(int num, int side, int times);
 
 void init(int n0, int n1, int times)
 {
+    end_sem = semget(IPC_PRIVATE, 1, 0666 | IPC_CREAT);
+    if(end_sem == -1)
+    {
+        perror("end_sem init failed");
+        exit(-1);
+    }
+
     entry_sem = semget(IPC_PRIVATE, 2, 0666 | IPC_CREAT);
     if (entry_sem == -1)
     {
@@ -108,7 +129,8 @@ void init(int n0, int n1, int times)
         exit(-1);
     }
 
-
+    v(entry_sem, 0);
+    v(entry_sem, 1);
 
     bridge_sem = semget(IPC_PRIVATE, 1, 0666 | IPC_CREAT);
     if (bridge_sem == -1)
@@ -181,7 +203,16 @@ void init(int n0, int n1, int times)
         }
     }
 
+    for(int i = 0; i <= (n1 + n0); ++i)
+        p(end_sem, 0);
     
+    for(int i = 0; i < 2; ++i)
+        kill_queue(i);
+
+    for(int i = 0; i < (n1 + n0 + 2); ++i)
+    {
+        wait(NULL);
+    }
 }
 
 int change_side(int side)
@@ -232,6 +263,7 @@ void pastux(int num, int side, int times)
     }
 
     printf("Pastux #%d: has died\n", num);
+    v(end_sem, 0);
     exit(0);
 }
 
